dd39a1abcc966b7cb5867ac55d88e848285143cc
Ellis <nardi0302@gmail.com>
2023-11-06 00:07:03 +0900

    Fix: Change imageg URL mapper function to correctly act in deploy env

--- src/utils/Animation.ts
+++ src/utils/Animation.ts
@@ -2,6 +2,7 @@ import { COLOR } from "../consts/Color";
 
 export class AnimationUtil {
   static getCurrentFrame(color: COLOR, frame: number) {
-    return `/public/balloon/${color}/${frame}.png`;
+    return new URL(`/public/balloon/${color}/${frame}.png`, import.meta.url)
+      .href;
   }
 }
