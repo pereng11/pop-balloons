7ede886f673594e5b4face78420a2788c878f406
Ellis <nardi0302@gmail.com>
2023-11-06 00:46:53 +0900

    Feat: Setup for PWA

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,52 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
+import { VitePWA } from "vite-plugin-pwa";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: "autoUpdate",
+      devOptions: {
+        enabled: true,
+      },
+      includeAssets: [
+        "/fabicons/favicon.ico",
+        "/fabicons/apple-touch-icon.png",
+      ],
+      manifest: {
+        name: "Pop Balloons",
+        short_name: "Pop Ballons",
+        description: "Pop Balloons App for coffee bet",
+        icons: [
+          {
+            src: "/fabicons/pwa-192x192.png",
+            sizes: "192x192",
+            type: "image/png",
+          },
+          {
+            src: "/fabicons/pwa-512x512.png",
+            sizes: "512x512",
+            type: "image/png",
+          },
+          {
+            src: "/fabicons/pwa-512x512.png",
+            sizes: "512x512",
+            type: "image/png",
+            purpose: "any",
+          },
+          {
+            src: "/fabicons/pwa-512x512.png",
+            sizes: "512x512",
+            type: "image/png",
+            purpose: "maskable",
+          },
+        ],
+        theme_color: "hsl(68, 100%, 91%)",
+        background_color: "hsl(68, 100%, 91%)",
+        display: "standalone",
+      },
+    }),
+  ],
+});
