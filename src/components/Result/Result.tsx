d7561de44cec5ffbc58571b75f2fca5a24252186
Ellis <nardi0302@gmail.com>
2023-11-12 15:53:02 +0900

    Fix: Stop audio on Result component unmount

--- src/components/Result/Result.tsx
+++ src/components/Result/Result.tsx
@@ -1,29 +1,22 @@
 import { useEffect, useMemo } from "react";
-import ReactCanvasConfetti from "react-canvas-confetti";
-import { defaultCanvasStyles, useConfetti } from "../../hooks/useConfetti";
 import style from "./Result.module.css";
 import { Link } from "react-router-dom";
 import { URI } from "../../consts/URI";
 import cheerAudioSrc from "./../../assets/audio/crowd-cheer.mp3";
 
 export const Result = () => {
-  const { fire, refConfetti } = useConfetti();
-
   const cheerAudio = useMemo(() => new Audio(cheerAudioSrc), []);
 
   useEffect(() => {
-    fire();
     cheerAudio.play();
-    const intervalId = setInterval(fire, 2000);
-    return () => clearInterval(intervalId);
-  }, [fire, cheerAudio]);
+    return () => {
+      cheerAudio.pause();
+      cheerAudio.currentTime = 0;
+    };
+  }, [cheerAudio]);
 
   return (
     <div className={style.container}>
-      <ReactCanvasConfetti
-        style={defaultCanvasStyles}
-        refConfetti={refConfetti}
-      />
       <p className={style.message}>YEAH!!!</p>
       <Link to={URI.INDEX} className={style.replay}>
         Replay
