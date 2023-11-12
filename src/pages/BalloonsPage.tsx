0b2cdbc2e4c9a85b573d253169a2ccb338814092
Ellis <nardi0302@gmail.com>
2023-11-12 15:53:32 +0900

    Feat: Move confetti effect from result to popping timing

--- src/pages/BalloonsPage.tsx
+++ src/pages/BalloonsPage.tsx
@@ -5,19 +5,27 @@ import { useCounterContext } from "../contexts/CounterContext";
 import { NumberUtil } from "../utils/Number";
 import style from "./BalloonsPage.module.css";
 import { Result } from "../components/Result/Result";
+import { defaultCanvasStyles, useConfetti } from "../hooks/useConfetti";
+import ReactCanvasConfetti from "react-canvas-confetti";
 
 export const BalloonsPage = () => {
   const { count } = useCounterContext();
   const [balloonsCount, setBalloonsCount] = useState(
     NumberUtil.generateRandomNumber(count, 2)
   );
+  const { fire, refConfetti } = useConfetti();
 
   const onBalloonPopped = () => {
+    fire();
     balloonsCount > 0 && setBalloonsCount((prev) => prev - 1);
   };
 
   return (
     <div className={style.container}>
+      <ReactCanvasConfetti
+        style={defaultCanvasStyles}
+        refConfetti={refConfetti}
+      />
       {balloonsCount > 0 ? (
         <Balloon
           key={balloonsCount}
